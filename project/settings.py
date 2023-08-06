1e1896d347607ff1f8c9b11c58e93fecbbaff377
MonkasteR <monkaster@gmail.com>
2023-08-06 10:53:20 +0300

    Обновил flatpages

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.flatpages.models.FlatPage',
+    'django.contrib.flatpages',
     'simpleapp.apps.SimpleappConfig',
     # 'simpleapp',
 ]
