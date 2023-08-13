b2fe205b54eb7a4fa78699b96a3dfc147d026608
MonkasteR <monkaster@gmail.com>
2023-08-13 13:05:32 +0300

    Сайт работает.

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
 
 from .filters import ProductFilter
 from .forms import ProductForm
@@ -14,40 +15,36 @@ class ProductsList(ListView):
     context_object_name = 'products'
     paginate_by = 2
 
-    # Переопределяем функцию получения списка товаров
     def get_queryset(self):
-        # Получаем обычный запрос
         queryset = super().get_queryset()
-        # Используем наш класс фильтрации.
-        # self.request.GET содержит объект QueryDict, который мы рассматривали
-        # в этом юните ранее.
-        # Сохраняем нашу фильтрацию в объекте класса,
-        # чтобы потом добавить в контекст и использовать в шаблоне.
         self.filterset = ProductFilter(self.request.GET, queryset)
-        # Возвращаем из функции отфильтрованный список товаров
         return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Добавляем в контекст объект фильтрации.
         context['filterset'] = self.filterset
         return context
 
 
 class ProductDetail(DetailView):
-    # Модель всё та же, но мы хотим получать информацию по отдельному товару
     model = Product
-    # Используем другой шаблон — product.html
     template_name = 'product.html'
-    # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'product'
 
 
-def create_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/products/')
-    form = ProductForm()
-    return render(request, 'product_edit.html', {'form': form})
+class ProductCreate(CreateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+
+class ProductUpdate(UpdateView):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product_edit.html'
+
+# Представление удаляющее товар.
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
